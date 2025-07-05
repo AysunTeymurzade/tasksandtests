60174ba34c649d1785ec993ee56e919e0db85859
AysunTeymurzade <aysunteymurzade0409@gmail.com>
2025-07-05 15:15:51 +0400

    Task

--- 4July1.py
+++ 4July1.py
@@ -1,10 +1,10 @@
 from datetime import datetime
 bugun = datetime.now()
 aylar = {
-    1: "Qışın başlanğıcı", 2: "Səbr ayı", 3: "Yenilənmə",
-    4: "Çiçəkləmə", 5: "İrəliləyiş", 6: "Yay həyəcanı",
-    7: "Günəşli çağ", 8: "Dincəlmə", 9: "Dəyişiklik",
-    10: "Sakitlik", 11: "Kədərli sükut", 12: "Qapanış"
+    1: "Başlanğıc", 2: "Səbir", 3: "Yenilənmə",
+    4: "Çiçəkləmə", 5: "İrəliləyiş", 6: "Həyəcan",
+    7: "Istilik", 8: "Dincəlmə", 9: "Dəyişiklik",
+    10: "Sakitlik", 11: "Sükut", 12: "Son"
 }
 gun = bugun.day
 ay = bugun.month
